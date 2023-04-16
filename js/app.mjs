8abf0dc6eb10aa507e5ffcd0ff361faef2cffb89
MARIENKOW <den.marienkow@gmail.com>
2023-04-17 01:38:37 +0300

    add 2 blocks and addapted it

--- js/app.mjs
+++ js/app.mjs
@@ -1,3 +1,37 @@
+// $(document).ready(function () {
+// 	$('.destinations__spoiler').slick({
+// 		arrows: true,
+// 		dots: true,
+// 		slidesToShow: 5,
+// 		slidesToScroll: 1,
+// 		speed: 500,
+// 		easing: 'ease-in-out',
+// 		infinite: false,
+// 		autoplay: true,
+// 		autoplaySpeed: 4000,
+// 		touchTreshold: 5,
+// 		centerMode: false,
+// 		focusOnSelect: false,
+// 		prevArrow: $('.buy__prevArrow'),
+// 		nextArrow: $('.buy__nextArrow'),
+// 		responsive: [
+// 			{
+// 				breakpoint: 956,
+// 				settings: {
+//                settings:'unslick',
+// 				}
+// 			},
+// 			{
+// 				breakpoint: 600,
+// 				settings: {
+// 					slidesToShow: 2,
+// 					slidesToScroll: 1,
+// 				}
+// 			}
+// 		]
+// 	});
+// });
+
 const burger = document.querySelector('.header__burger');
 if(burger){
    burger.addEventListener('click',event=>{
@@ -14,5 +48,22 @@ window.addEventListener('resize',event=>{
    if(document.body.classList.contains('_burgerOpen')){
       document.body.classList.remove('_burgerOpen')
       document.body.style.overflow='';
+   }
+})
+const footer = document.querySelector('footer');
+const header = document.querySelector('header');
+window.addEventListener('scroll',event=>{
+   const footerTop = footer.getBoundingClientRect().top;
+   const windowHeight = window.innerHeight
+   if(footerTop - windowHeight < 0){
+      header.style.cssText = `
+      transition:.3s;
+      transform:translateY(-120%);
+      `
+   }else{
+      header.style.cssText = `
+      transition:.3s;
+      transform:translateY(0);
+      `
    }
 })
